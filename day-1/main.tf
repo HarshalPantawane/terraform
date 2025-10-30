0e474a62010c432295d64585914a95ff045048c4
Harshal Pantawane <hmpantawane2214@gmail.com>
2025-10-31 00:59:51 +0530

    day-1

--- day-1/main.tf
+++ day-1/main.tf
@@ -10,3 +10,10 @@ resource "aws_vpc" "name" {
  }
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "public subnet"
+  }
+}
